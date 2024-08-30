60f3d5af3b038efe6da4035234f3024374883cda
crimun <crimun@gmail.com>
2024-08-30 17:15:52 -0400

    Iniciando creación de vista Login

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<!-- Esto es una vista que despues puede llegar a ser insertada en otra vista como un componente -->
+
+<template>
+    <div>
+        Hola desde mi vista Login
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'Login'
+    }
+</script>
+
+<!-- Estas eqtiquetas son iopcionales <style> </style> donde va el CSS-->
