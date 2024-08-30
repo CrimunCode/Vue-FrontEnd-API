137ea39f5f5eb57fa4b76d670f0ccb1d5388f2a1
crimun <crimun@gmail.com>
2024-08-30 17:37:39 -0400

    Actualización del App.vue (seañadio vistas User.vue y login.vue

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,43 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import MiComponente from './components/MiComponente.vue'
-import MySelect from './components/MySelect.vue'
-import ProductList from './components/ProductList.vue'
-import MyTable from './components/MyTable.vue'
-</script>
-
+<!-- Estructura logica en VUE 2 (Option) y en este caso permitido en VUE 3 (Composition)  -->
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <MyTable />
-  
-  <HelloWorld msg="Vite + Vue" />
-  <MiComponente />
-  <br>
-  <MySelect />
-  <br>
-  <ProductList Id="id" Nombre="nombre_prod" MyClass="table table-striped table-hover"/>
-  <br>
-  <ProductList Id="item" Nombre="Nombre Usuario" MyClass="table table-striped"/>
 
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script>
+
+  export default {
+    name: 'App',
+    // Setear los componentes importados
+    components: {
+
+    },
+    // Datos que son seteados dentro de la vista
+    // 
+    data(){
+      return{
+
+      }
+    },
+    created(){
+      
+    },
+    // Estos Métodos propios de la vista
+    methods:{ 
+        
+
+      },
+      // Propiedades Computadas
+      computed: {
+        
+      },
+
+  }
+
+
+</script>
+
+
+
+<style>
+
 </style>
